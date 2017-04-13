6e2ccdb97551fcbe50d4659250737df05f702510
jonghoon <jhwgnu@hanmail.net>
2017-04-14 06:23:34 +0900

    Game 컴포넌트로 state control 넘김

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
-import Board from './components/Board'
 import Square from './components/Square'
-import board from './reducers'
 import square from './reducers'
+import Board from './components/Board'
+import board from './reducers'
+import Game from './components/Game'
+import game from './reducers'
 
-const store = createStore(board)
+const store = createStore(game)
 const rootEl = document.getElementById('root')
 
 const render = () => ReactDOM.render(
-    <Board />,
+    <Game />,
+    // <Board />,
     // <Square />,
   // <Square
   //
